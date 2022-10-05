135efe38739bd4d7357518f4900c2808a81de155
MeheheCedy22 <96355171+MeheheCedy22@users.noreply.github.com>
2022-10-05 11:41:56 +0200

    5/ka nedokoncena, obe otestovat

---
+++ zadania/tretieZadanie/121193_2022_3_5.c
@@ -0,0 +1,21 @@
+//nedokoncene
+#include <stdio.h>
+
+
+int main()
+{
+    int f,N=0;
+    while(N>=0)
+    {
+        scanf("%d", &N);
+    }
+
+    for(int i=0;i<N;i++)
+    {
+
+    }
+
+    printf("\n%d",)
+
+    return 0;
+}
