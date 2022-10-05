01bb2b252f056dec1547cee0acdb43867243b1d4
MeheheCedy22 <96355171+MeheheCedy22@users.noreply.github.com>
2022-10-05 10:49:52 +0200

    test, potom vymeniť/vymazať

---
+++ zadania/tretieZadanie/121193_2022_3_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    float array[N];
+    float min,max;
+    scanf("%d",&N);
+    printf("\n");
+    
+    for (int i=0; i<N;i++)
+    {
+        scanf("%f",&array[i]);
+    }
+
+    min=max=array[0];
+
+    for (int j=0;j<N;j++)
+    {
+        if(min>array[j])
+        {
+            min=array[j];
+        }
+        if(max<array[j])
+        {
+            max=array[j];
+        }
+    }
+
+    printf("\nMinimum: %.2f\n",min);
+    printf("\nMaximum: %.2f\n",max);
+
+    return 0;
+}
